69ae3c95c3cd24238fa57559bc25fae4cd3d93bc
AinaPatrick <rsolofoniainapatrick4@gmail.com>
2025-08-11 12:11:55 +0300

    connection base de données

--- config/Config.php
+++ config/Config.php
@@ -1,22 +1,26 @@
 <?php
 
-class Config {
+class Config
+{
     private $host = "localhost";
     private $dbname = "blog_patrick";
     private $user = "debian-sys-maint";
     private $mdp = "NouveauMotDePasse";
     private $pdo;
+    private $charset = 'utf8mb4';
 
-    public function __construct() {
+    public function __construct()
+    {
         try {
-            $this->pdo = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname.";charset=utf8", $this->user, $this->mdp);
-           $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
+            $this->pdo = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname . ";charset=".$this->charset, $this->user, $this->mdp);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
             die("Erreur de connexion à la base de données : " . $e->getMessage());
         }
     }
 
-    public function getConnection() {
+    public function getConnection()
+    {
         return $this->pdo;
     }
 }
